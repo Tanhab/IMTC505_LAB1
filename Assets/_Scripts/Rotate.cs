d5af3ed82e0433a4675a27eeebd782c8adfefe79
SARKER <tanhab@ead.ubc.ca>
2024-09-23 18:01:30 -0700

    Rotate obstacle and script done

---
+++ Assets/_Scripts/Rotate.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    private bool shouldRotate = false; // Toggle for rotation after hitting robot
+
+ 
+    void Update()
+    {
+        if (shouldRotate)
+        {
+            transform.Rotate(Vector3.up * 100f * Time.deltaTime); // Rotate on Y-axis
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        shouldRotate = true;
+    }
+}
